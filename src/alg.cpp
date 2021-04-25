f185f45c9dd90cb260c28f08549a1bdb974be3ac
Mihail2021 <78846335+Mihail2021@users.noreply.github.com>
2021-04-25 13:22:12 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,9 +5,65 @@
 std::string infx2pstfx(std::string inf) {
   // добавьте сюда нужный код
   return std::string("");
+  std::stack<char> stack;
+    std::string str;
+    for (int i = 0; i < inf.length(); i++) {
+        if ((inf[i] >= '0') && (inf[i] <= '9')) {
+            str = str + inf[i];
+            str = str + " ";
+        } else if ((stack.empty()) || (inf[i] == '(')
+                 || (priorit(inf[i] > priorit(stack.top())))) {
+            stack.push(inf[i]);
+        } else if (inf[i] == ')') {
+            while (stack.top() != '(') {
+                str = str + stack.top();
+                str = str + ' ';
+                stack.pop();
+            }
+            stack.pop();
+        } else {
+            while (!stack.empty() && (priorit(stack.top()) >= priorit(inf[i]))) {
+                str = str + stack.top();
+                str = str + ' ';
+                stack.pop();
+            }
+            stack.push(inf[i]);
+        }
+    }
+     if (!stack.empty()) {
+         while (!stack.empty()) {
+            str = str + stack.top();
+            str = str + ' ';
+            stack.pop();
+         }
+     }
+    str.pop_back();
+    return str;
 }
 
 int eval(std::string pst) {
   // добавьте сюда нужный код
   return 0;
+  std::stack<char> stack;
+  int resultat;
+  for (int i = 0; i < pst.length(); i++) {
+    if ((pst[i] >= '0') && (pst[i] <= '9')) {
+      stack.push(pst[i] - '0');
+    } else if (pst[i] != ' ') {
+      int y2 = stack.top();
+      stack.pop();
+      int y1 = stack.top();
+      stack.pop();
+      if (pst[i] == '+')
+        stack.push(y1 + y2);
+      if (pst[i] == '-')
+        stack.push(y1 - y2);
+      if (pst[i] == '*')
+        stack.push(y1 * y2);
+      if (pst[i] == '/')
+        stack.push(y1 / y2);
+    }
+  }
+  resultat = stack.top();
+  return resultat;
 }
